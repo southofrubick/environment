4e5cc0c545ef7c3aa6f0803b4141be2505182690
MartinWJonsson <martin.jonsson@octanelending.com>
2024-02-27 18:54:43 +0100

    update plugins to use catppuccin

--- nvim/after/plugin/gitblame.lua
+++ nvim/after/plugin/gitblame.lua
@@ -2,11 +2,11 @@ require('blame_line').setup({
     show_in_visual = false,
     show_in_insert = false,
     prefix = '',
-    template = '    <author> • <summary> • <author-time>',
+    template = '    <author> • <summary> • <author-time> • <commit-short>',
     date = {
         relative = true,
         format = "%d-%m-%y",
     },
-    hl_group = "Debug",
-    delay = 2000,
+    hl_group = "Comment",
+    delay = 1000,
 })
