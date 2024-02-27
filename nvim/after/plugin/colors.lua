49cdadb644c710280ead7b969f59461a35561b7d
MartinWJonsson <martin.jonsson@octanelending.com>
2024-02-27 18:54:43 +0100

    update plugins to use catppuccin

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,13 +1,33 @@
 local cmd = vim.cmd
 local api = vim.api
+require('catppuccin').setup({
+    compile = {
+        enabled = true,
+        path = vim.fn.stdpath "cache" .. "/catppuccin",
+    },
+    transparent_background = true,
+    dim_inactive = {
+        enabled = false,
+    },
+    integrations = {
+        native_lsp = {
+            enabled = true,
+        },
+        hop = true,
+        mason = true,
+        telescope = {
+            enabled = true,
+        },
+        treesitter = true,
+        treesitter_context = true,
+        lsp_trouble = true,
+        gitgutter = true,
+        which_key = true,
+        illuminate = {
+            enabled = true,
+            lsp = true,
+        },
+    },
+})
 
-function ColorMyPencils(color)
-    color = color or "catppuccin-mocha"
-    cmd.colorscheme(color)
-
-    api.nvim_set_hl(0, "Normal", { bg = "none" })
-    api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-end
-
-ColorMyPencils()
+vim.cmd [[colorscheme catppuccin-mocha]]
