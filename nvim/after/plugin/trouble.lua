86eac0f4fe3852b087e61bea72cdf97bfec2124f
MartinWJonsson <martin.jonsson@octanelending.com>
2024-10-11 16:07:12 +0200

    update trouble, new branch always from origin

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -15,11 +15,11 @@ require('trouble').setup({
         close = "q", -- close the list
         cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
         refresh = "r", -- manually refresh
-        jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
+        jump = {"<tab>"}, -- jump to the diagnostic or open / close folds
         open_split = {}, -- open buffer in new split
         open_vsplit = {}, -- open buffer in new vsplit
         open_tab = {}, -- open buffer in new tab
-        jump_close = {"o"}, -- jump to the diagnostic and close the list
+        jump_close = {"<cr>", "o"}, -- jump to the diagnostic and close the list
         toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
         switch_severity = "s", -- switch "diagnostics" severity filter level to HINT / INFO / WARN / ERROR
         toggle_preview = "P", -- toggle auto_preview
@@ -31,10 +31,13 @@ require('trouble').setup({
         previous = "k", -- previous item
         next = "j" -- next item
     },
+    keys = {
+        ["<cr>"] = "jump_close",
+    },
     indent_lines = true, -- add an indent guide below the fold icons
     auto_open = false, -- automatically open the list when you have diagnostics
     auto_close = false, -- automatically close the list when you have no diagnostics
-    auto_preview = false, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
+    auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
     auto_fold = true, -- automatically fold a file trouble list at creation
     auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
     signs = {
@@ -45,5 +48,6 @@ require('trouble').setup({
       information = "",
       other = "",
     },
-    use_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
+    use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
+    focus = true,
 })
